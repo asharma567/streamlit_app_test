a361a83650f06009b7b855575a0bed93efa2949e
asharma567 <asharma567567@gmail.com>
2022-08-29 19:05:15 -0700

    fixed bugs and renamed columns and reset the default setting

--- leaderboard_shap.py
+++ leaderboard_shap.py
@@ -15,7 +15,6 @@ def st_shap(plot, height=None):
     shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
     components.html(shap_html, height=height)
 
-st.title("Boba Leaderboard")
 
 def scale_and_standardize(df):
     from sklearn.preprocessing import StandardScaler    
@@ -23,21 +22,6 @@ def scale_and_standardize(df):
     scaler.fit(df)
     scaled_df = scaler.transform(df)
     return scaled_df
-
-#load joblib
-df_M_character = joblib.load('../df_M_character.joblib')
-df_M_character_scale = scale_and_standardize(df_M_character)
-author_idx_lookup = dict([(name, idx) for idx, name in enumerate(df_M_character.index)])
-
-explainer = joblib.load('../explainer.joblib')
-shap_values = joblib.load('../shap_values.joblib')
-
-
-X = pd.DataFrame(
-    df_M_character_scale, 
-    columns=df_M_character.columns)
-
-
 
 def aggrid_interactive_table(df: pd.DataFrame):
     """Creates an st-aggrid interactive table based on a dataframe.
@@ -66,21 +50,40 @@ def aggrid_interactive_table(df: pd.DataFrame):
 
     return selection
 
-#change to leaderboard
-leaderboard = pd.read_csv("leaderboard.csv")
+st.title("Boba Leaderboard")
 
-selection = aggrid_interactive_table(df=leaderboard)
+#load joblib
+df_M_character = joblib.load('df_M_character.joblib')
+df_M_character_scale = scale_and_standardize(df_M_character)
+author_idx_lookup = dict([(name, idx) for idx, name in enumerate(df_M_character.index)])
+explainer = joblib.load('explainer.joblib')
+shap_values = joblib.load('shap_values.joblib')
+
+X = pd.DataFrame(
+    df_M_character_scale, 
+    columns=df_M_character.columns)
+
+
+df_leaderboard = pd.read_csv("leaderboard.csv")
+df_leaderboard.rename(columns = {'Unnamed: 0':'Rank', 'author':'member'}, inplace=True)
+selection = aggrid_interactive_table(df=df_leaderboard)
+
+
+
 
 if selection:
-
-
-    auth = selection["selected_rows"][0]['author']
+    try: 
+        auth = selection["selected_rows"][0]['member']
+    except:
+        auth = df_leaderboard['member'].iloc[0]
     st.write(auth)
     author_idx = author_idx_lookup.get(auth)
-
-
     # visualize the first prediction's explaination with default colors
     st_shap(shap.force_plot(explainer.expected_value, shap_values[author_idx,:], X.iloc[author_idx,:]))
+
+
+
+
 
 
 
